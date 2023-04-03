029da591f3cc0a71305db01a9f5a6f8481f471cb
Romain Hauet <hr202541@localhost.localdomain>
2023-04-03 09:52:04 +0200

    init db

---
+++ initDB/initalisation.sql
@@ -0,0 +1,41 @@
+drop table if exists utilisateur cascade;
+drop table if exists projet cascade;
+
+create table utilisateur(
+    nom varchar(20),
+    password varchar(20)
+);
+
+create table projet(
+    utilisateur varchar(20),
+    nom varchar(20),
+    image varchar(500),
+    description varchar(500),
+    competence varchar(500)
+);
+
+insert into utilisateur values(
+    "LIAM",
+    "123"
+);
+
+insert into utilisateur values(
+    "ROMAIN",
+    "123"
+);
+
+insert into projet values(
+    "LIAM",
+    "Les Aventuriers du rails de coke",
+    "Lia.txt",
+    "creer le jeu les aventurier du rail",
+    "massage"
+);
+
+insert into projet values(
+    "ROMAIN",
+    "Serpent numérique",
+    "Bite.txt",
+    "creer le jeu prefere de plp ou se tailler les veines",
+    "dessin et compter jusque 21"
+);
