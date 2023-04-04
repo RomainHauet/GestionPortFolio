0668451772c6c9047ffd7054ba6512cf1c079ad8
Liam <liam.deniau@etu.univ-lehavre.fr>
2023-04-04 10:05:31 +0200

    ryry

--- PageConnection.php
+++ PageConnection.php
@@ -17,35 +17,10 @@
     $db = DB::getInstance();
 
     // On récupère les projet de la base de données
-    switch($_REQUEST)
-    {
-        case isset($_REQUEST['projet']):
-            $projet = $db->getProjets();
-            break;
-
-        case isset($_REQUEST['cv']):
-            $CV = $db->getCV();
-            break;
-
-        case isset($_REQUEST['contact']):
-            $projet = $db->getContact();
-            break;
-
-        case isset($_REQUEST['accueil']):
-            $projet = $db->getAccueil();
-            break;
-
-        case isset($_REQUEST['competence']):
-            $projet = $db->getCompetence();
-            break;
-        case isset($_REQUEST['credit']):
-            $projet = $db->getCredit();
-            break;
-    }
+    $projet = $db->getProjets();
 
     // generation d'une vue a partir du template
     echo $tpl->render( array(
         "titre" => "Accueil du site",
-        "typeLecture" => "lecture",
         "projets" => $projet)); // a remplacer
 ?>
