12f0f9a81e8ab6165e81e75f62918c303f1db53e
RomainHauet <romain.hauet@etu.univ-lehavre.fr>
2023-04-09 22:09:54 +0200

    ajout inscription

--- ajoutDB.php
+++ ajoutDB.php
@@ -138,9 +138,6 @@
         }
     }
 
-    // pour pouvoir utiliser le loader de twig
-    require_once( "./Twig/lib/Twig/Autoloader.php" );
-
     Twig_Autoloader::register();
     // On indique que les templates seront charges depuis ./tpl/ 
     $twig = new Twig_Environment( new Twig_Loader_Filesystem("./tpl"));
